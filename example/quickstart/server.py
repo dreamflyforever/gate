6aca2c24d0f278d5f6c1c8e2fbbd0490a224bee3
jim <1150660744@qq.com>
2022-03-08 17:54:59 +0800

    add mqtt to send message
    Signed-off-by: jim <1150660744@qq.com>

--- example/quickstart/server.py
+++ example/quickstart/server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
 
 import time
 import asyncio
@@ -7,8 +6,29 @@ import websockets
 import websocket as ws
 import logger
 import os
+from paho.mqtt import client as mqtt_client
+import random
 
 main = r"./fos"
+
+broker = '127.0.0.1'
+port = 1883
+topic = "mqtt"
+
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 async def hello(websocket):
     name = await websocket.recv()
@@ -23,10 +43,12 @@ async def hello(websocket):
         answer = rv.read()
         print(answer)
         await websocket.send(answer)
+        client.publish(topic, answer)
 
 async def main():
     async with websockets.serve(hello, "localhost", 8765, max_size = 10752000):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
+    client = connect_mqtt()
     asyncio.run(main())
