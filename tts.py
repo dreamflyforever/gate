becc5f155128d71b55c901f30b4027e78a771bc9
jim <1150660744@qq.com>
2022-05-19 17:30:19 +0800

    add url return
    Signed-off-by: jim <1150660744@qq.com>

--- tts.py
+++ tts.py
@@ -7,7 +7,7 @@ import time
 ws = websocket.WebSocket()
 ws.connect("ws://192.168.73.249:8766", max_size = 10752000)
 
-ws.send("Hello world test")
+ws.send("EXQUISITE ORDER AND UNIVERSAL WITH ETERNAL LIFE AND LIGHT THIS IS THE FAITH AND EFFORT OF THE SCHOOLS OF CRYSTAL AND YOU MAY DESCRIBE AND COMPLETE THEIR WORK QUITE LITERALLY BY TAKING ANY VERSES OF CHAUCER IN HIS TENDER MOOD AND OBSERVING HOW HE INSISTS ON THE CLEARNESS AND BRIGHTNESS FIRST AND THEN ON THE ORDER")
 '''
 audio = ws.recv_frame()
 print(audio)
@@ -19,6 +19,7 @@ with open('./test.mp3', 'wb') as f:
             print('exit')
             break
         print(type(audio.data), len(audio.data))
+        print(audio.data)
         print('-------------------------------')
         f.write(audio.data)
 
