14bae850fd73c0c8f23f2361e56df8bd9e834af6
jim <1150660744@qq.com>
2022-05-20 18:17:13 +0800

    add long connnect function client
    Signed-off-by: jim <1150660744@qq.com>

--- tts.py
+++ tts.py
@@ -7,19 +7,31 @@ import time
 ws = websocket.WebSocket()
 ws.connect("ws://192.168.73.249:8766", max_size = 10752000)
 
+
 ws.send("EXQUISITE ORDER AND UNIVERSAL WITH ETERNAL LIFE AND LIGHT THIS IS THE FAITH AND EFFORT OF THE SCHOOLS OF CRYSTAL AND YOU MAY DESCRIBE AND COMPLETE THEIR WORK QUITE LITERALLY BY TAKING ANY VERSES OF CHAUCER IN HIS TENDER MOOD AND OBSERVING HOW HE INSISTS ON THE CLEARNESS AND BRIGHTNESS FIRST AND THEN ON THE ORDER")
-'''
-audio = ws.recv_frame()
-print(audio)
-'''
-with open('./test.mp3', 'wb') as f:
+
+with open('./test.wav', 'wb') as f:
     while (True):
         audio = ws.recv_frame()
         if len(audio.data) == 2 and audio.data == b'\x03\xe8':
             print('exit')
             break
         print(type(audio.data), len(audio.data))
-        print(audio.data)
+        #print(audio.data)
+        print('-------------------------------')
+        f.write(audio.data)
+
+print('rsend')
+ws.send("EXQUISITE ORDER AND UNIVERSAL WITH ETERNAL LIFE AND LIGHT THIS IS THE FAITH AND EFFORT OF THE SCHOOLS OF CRYSTAL AND YOU MAY DESCRIBE AND COMPLETE THEIR WORK QUITE LITERALLY BY TAKING ANY VERSES OF CHAUCER IN HIS TENDER MOOD AND OBSERVING HOW HE INSISTS ON THE CLEARNESS AND BRIGHTNESS FIRST AND THEN ON THE ORDER")
+
+with open('./2.wav', 'wb') as f:
+    while (True):
+        audio = ws.recv_frame()
+        if len(audio.data) == 2 and audio.data == b'\x03\xe8':
+            print('exit')
+            break
+        print(type(audio.data), len(audio.data))
+        #print(audio.data)
         print('-------------------------------')
         f.write(audio.data)
 
